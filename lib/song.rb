39dc13c83887c0d82ecd478a1f8c475b09116f8b
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-16 18:59:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,10 @@
 
 class Song
   
-  
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
-  end
-
-  def self.all 
-    @@all 
   end
 
   def self.new_by_filename(filename)
