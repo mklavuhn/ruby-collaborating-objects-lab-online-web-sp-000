f83e7b857200d2070ccfa5a3485a02c5922fbe71
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-16 19:01:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,11 +7,11 @@ class Song
     @name = name
   end
 
-  def self.new_by_filename(filename)
-    artist, song = filename.split(" - ")
-    new_song = self.new(song)
-    new_song.artist_name = artist
-    new_song
+  def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
+    song
   end
 
   def artist_name=(name)
